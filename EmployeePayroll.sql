e9b0da353cd95f638cfbf9a35f14ba7a6a4a0a3c
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-26 17:49:34 +0530

    Added to CreateEmployeePayrollTable

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -4,3 +4,15 @@
 create database employee_Payroll
 
 use Employee_Payroll;
+
+--Create Employee_Payroll Table
+create table employee_Payroll(
+--id is incremented by one and stating from 1, primary key(unique and not null)
+id int identity(1,1) primary key,
+--name is not null
+name varchar(200) not null,
+--salary is not null
+salary int not null,
+--startdate stores dates which is not null
+startdate date not null,
+);
