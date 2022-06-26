ae8d267c949f7647171ff78c328c1c6744db34eb
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-26 18:42:03 +0530

    Added to Retievedata

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -5,7 +5,7 @@ create database employee_Payroll
 
 use Employee_Payroll;
 
---Create Employee_Payroll Table
+--UC2  Create Employee_Payroll Table
 create table employee_Payroll(
 --id is incremented by one and stating from 1, primary key(unique and not null)
 id int identity(1,1) primary key,
@@ -13,12 +13,15 @@ id int identity(1,1) primary key,
 name varchar(200) not null,
 --salary is not null
 salary int not null,
---startdate stores dates which is not null
-startdate date not null,
+--startDate stores dates which is not null
+startDate date not null,
 );
 
---Insert Data into table employee_payroll
+--UC3 Insert Data into table employee_payroll
 insert into employee_Payroll values
 ('Sudhanshu', 60000, '2019-10-12'),
 ('Vivek', 80000, '2019-04-05'),
 ('Sonu', 90000, '2004-05-09');
+
+--UC4 Retrieve all data we have inserted into the employee_Payroll
+select * from employee_Payroll
